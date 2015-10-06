0145b9d1d58b211339c41060fbe18d54c7c3aec9
Gaston Cocce <gcocce@gmail.com>
2015-10-06 08:07:45 -0300

    Cambios al game controller y al cliente

--- BaseNet/src/basenetgame/client/CommModule.java
+++ BaseNet/src/basenetgame/client/CommModule.java
@@ -1,34 +1,55 @@
 package basenetgame.client;
 
+import java.io.IOException;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class CommModule {
 	
-	GameController gc;
-	int estado;
-	Socket s;
+	public enum Estado{DEFAULT, CONECTADO, ERROR_CONEXION};
+	
+	Estado estado;
+	Socket socket;
+	GameController gc;	
 	PacketManager pktmgr;
 	
 	public CommModule(GameController g){
-		estado=0;
+		estado=Estado.DEFAULT;
+		socket=null;
+		pktmgr=null;
 		this.gc=g;
 	}
 	
 	public boolean IniciarConexion(String host, int puerto){
 		
-		
-		
-		
-		// Si se conecta correctamente
-		pktmgr= new PacketManager();
-		pktmgr.addListener(gc);		
-		pktmgr.start();
-
-		
-		return true;
+		try {
+			// Se intenta crear el socket para establecer la conexión
+			socket = new Socket(host, puerto);
+			
+	        // Muestra la dirección remota de la conexión con el servidor
+	        System.out.println("Conectado a " + socket.getRemoteSocketAddress());				
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        
+		if (socket!=null){
+			// Si se conecta correctamente creamos el PacketManager
+			pktmgr= new PacketManager();
+			pktmgr.addListener(gc);		
+			pktmgr.start();
+			
+			estado=Estado.CONECTADO;
+			
+			return true;
+		}else{
+			estado=Estado.ERROR_CONEXION;
+			return false;
+		}
 	}
 	
-	public int getEstado(){
+	public Estado getEstado(){
 		return estado;
 	}
 	
--- BaseNet/src/basenetgame/client/GameController.java
+++ BaseNet/src/basenetgame/client/GameController.java
@@ -4,8 +4,37 @@ import basenetgame.common.Packet;
 
 public class GameController extends Thread implements PacketListener{
 
+	public GameController(){
+
+	}
+	
+	private boolean initComm(String host, int puerto){
+		CommModule comModule = new CommModule(this);	
+		
+		if (comModule.IniciarConexion( host, puerto)){
+			System.out.println("Se inicio correctamente la comunicación");
+			return true;
+		}else{
+			System.out.println("Error al iniciar la comunicación");
+			return false;
+		}		
+	}
+	
 	public void run(){
 		
+		do{
+			initComm("localhost",5000);
+
+			
+			try {			
+				// Sleep de 0 milisegundos para dejar que el sistema operativo
+				// de paso a otro proceso o thread en este punto
+				Thread.sleep(0);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}			
+		}while(true);
 	}
 
 	@Override
