19d625639cbe99d7c5efb9567ff5285f71c49bb3
gcocce <gastoncocce@hotmail.com>
2015-11-12 07:36:16 -0300

    Agregamos archivos batch para correr los programas.

---
+++ BaseNet/bin/client.bat
@@ -0,0 +1,4 @@
+ECHO OFF
+title "Client"
+REM %1 es el host  %2 el puerto y %3 el nombre de usuario
+java basenetgame.client.Client %1 %2 %3
---
+++ BaseNet/bin/server.bat
@@ -0,0 +1,4 @@
+ECHO OFF
+title "Server"
+REM %1 es el puerto
+java basenetgame.server.Server %1
